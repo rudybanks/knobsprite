6aa13fb3d26e10490c8a57177e03f48db85e9784
Rudy Banks <rudolphbanks@gmail.com>
2024-07-03 11:45:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,10 +27,10 @@ st.markdown(
 
 
 # Create a variable to hold the image size
-image_size = st.sidebar.slider("Knob Output Size", min_value=25, max_value=400, value=200)
+image_size = st.slider("Knob Output Size", min_value=25, max_value=400, value=200)
 
 # Create a variable to hold the output filename
-output_filename = st.sidebar.text_input("Use Default or Rename", value="sprite.png")
+output_filename = st.text_input("Use Default or Rename", value="sprite.png")
 
 # Create a function to handle file selection
 @st.cache
